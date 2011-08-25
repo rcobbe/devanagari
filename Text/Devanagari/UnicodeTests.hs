b5ae040180600b9ca650bce6593b41c205af1c5c
Richard Cobbe <rcobbe@rcobbe.net>
2011-08-25 06:44:22 -0400

    Text.Devanagari.UnicodeTests: deleted commented-out, obsolete tests

--- Text/Devanagari/UnicodeTests.hs
+++ Text/Devanagari/UnicodeTests.hs
@@ -32,30 +32,3 @@ mkUtoPTest (label, unicode, phonemes) =
 mkPtoUTest :: (String, String, [P.Phoneme]) -> Test
 mkPtoUTest (label, unicode, phonemes) =
   label ~: (U.fromPhonemic phonemes ~?= Just unicode)
-
-{-
-    ["from unicode; CV*C, with implicit vowel" ~:
-     (U.toPhonemic "वनम्") ~?=
-     Just ([P.V, P.A, P.N, P.A, P.M], ""),
-     "from unicode; with samyoga" ~:
-     (U.toPhonemic "कूपस्य") ~?=
-     Just ([P.K, P.UU, P.P, P.A, P.S, P.Y, P.A], ""),
-     "from unicode: initial vowel with samyoga" ~:
-     (U.toPhonemic "अश्वेषु") ~?=
-     Just ([P.A, P.PalS, P.V, P.E, P.RetS, P.U], ""),
-     "to unicode: CV*, no implicit vowels" ~:
-     (U.fromPhonemic [P.K, P.UU, P.P, P.E, P.RetS, P.U]) ~?=
-     Just "कूपेषु",
-     "to unicode: CV*C, no implicit vowels" ~:
-     (U.fromPhonemic [P.K, P.UU, P.P, P.AA, P.T]) ~?=
-     Just "कूपात्",
-     "to unicode: CV*C with implicit vowel" ~:
-     (U.fromPhonemic [P.V, P.A, P.N, P.A, P.M]) ~?=
-     Just "वनम्",
-     "to unicode: with samyoga" ~:
-     (U.fromPhonemic [P.K, P.UU, P.P, P.A, P.S, P.Y, P.A]) ~?=
-     Just "कूपस्य",
-     "to unicode: initial vowel with samyoga" ~:
-     (U.fromPhonemic [P.A, P.PalS, P.V, P.E, P.RetS, P.U]) ~?=
-     Just "अश्वेषु"]))
--}
