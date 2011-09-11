d8c0d8d4698345956a6292a9feed5cbc64bf09e2
Richard Cobbe <rcobbe@rcobbe.net>
2011-09-10 19:42:59 -0700

    Extended devtrans executable to handle both conversions

--- Main.hs
+++ Main.hs
@@ -19,7 +19,7 @@ In either case, output followed by newline.
 -}
 
 import qualified System.Exit as Exit
--- import qualified System.Environment.UTF8 as Env
+import qualified System.Environment.UTF8 as Env
 -- Env.getArgs returns argv[1], argv[2], ...
 import System.IO
 
@@ -28,12 +28,20 @@ import qualified Text.Devanagari.Unicode as U
 import qualified Text.Devanagari.Velthuis as V
 
 main :: IO ()
-main = doIfNotEof (
+main =
+  do args <- Env.getArgs
+     case args of
+       ["--to-velthuis"] -> mainLoop unicodeToVelthuis
+       ["--to-unicode"] -> mainLoop velthuisToUnicode
+       _ -> printUsage
+
+mainLoop :: (String -> Maybe String) -> IO ()
+mainLoop convert = doIfNotEof (
   do line <- getLine
-     case (fmap V.fromSegments (U.toSegments line)) of
+     case convert line of
        Just result -> putStrLn ("(ok |" ++ result ++ "|)")
        Nothing -> putStrLn "(error \"invalid unicode\")"
-     main
+     mainLoop convert
      )
 
 doIfNotEof :: IO () -> IO ()
@@ -41,3 +49,15 @@ doIfNotEof action =
   do eof <- isEOF
      if eof then return () else action
 
+unicodeToVelthuis :: String -> Maybe String
+unicodeToVelthuis str =
+  fmap V.fromSegments (U.toSegments str)
+
+velthuisToUnicode :: String -> Maybe String
+velthuisToUnicode str =
+  fmap U.fromSegments (U.toSegments str)
+
+printUsage :: IO ()
+printUsage =
+  do putStrLn "devtrans: convert between Velthuis & Unicode representations"
+     putStrLn "Usage: devtrans { --to-velthuis | --to-unicode }"
